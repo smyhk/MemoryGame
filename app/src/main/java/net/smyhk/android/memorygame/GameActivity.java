a08d8b7677197f692cbe2f23f701a22ea39e5268
Steve Kedzie <seal.team.smyhk@gmail.com>
2017-03-07 22:34:20 -0800

    implement sequence method

--- app/src/main/java/net/smyhk/android/memorygame/GameActivity.java
+++ app/src/main/java/net/smyhk/android/memorygame/GameActivity.java
@@ -2,11 +2,14 @@ package net.smyhk.android.memorygame;
 
 import android.media.MediaPlayer;
 import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class GameActivity extends AppCompatActivity implements View.OnClickListener {
     TextView mScoreTextView;
@@ -20,8 +23,8 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
     Button mButtonReplay;
 
     // for threading
-    int difficultyLevel = 3;
-    int[] sequenceToCopy = new int[100]; // holds randomly generated sequence
+    int mDifficultyLevel = 3;
+    int[] mSequenceToCopy = new int[100]; // holds randomly generated sequence
 
     private Handler mHandler;
     // playing sequence?
@@ -44,10 +47,57 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         final MediaPlayer mp2 = MediaPlayer.create(this, R.raw.button_2);
         final MediaPlayer mp3 = MediaPlayer.create(this, R.raw.button_3);
         final MediaPlayer mp4 = MediaPlayer.create(this, R.raw.button_4);
+
+        // reference UI elements
+        mScoreTextView = (TextView) findViewById(R.id.textScore);
+        mScoreTextView.setText(getString(R.string.score_text, mPlayerScore));
+
+        mDifficultyTextView = (TextView) findViewById(R.id.textDifficulty);
+        mDifficultyTextView.setText(getString(R.string.difficulty_text, mDifficultyLevel));
+
+        mWatchGoTextView = (TextView) findViewById(R.id.textWatchGo);
+
+        mButtonOne = (Button) findViewById(R.id.button1);
+        mButtonOne.setOnClickListener(this);
+
+        mButtonTwo = (Button) findViewById(R.id.button2);
+        mButtonTwo.setOnClickListener(this);
+
+        mButtonThree = (Button) findViewById(R.id.button3);
+        mButtonThree.setOnClickListener(this);
+
+        mButtonFour = (Button) findViewById(R.id.button4);
+        mButtonFour.setOnClickListener(this);
+
+        mButtonReplay = (Button) findViewById(R.id.buttonReplay);
+        mButtonReplay.setOnClickListener(this);
+
+        // this code defines the thread
+        mHandler = new Handler() {
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+
+                if (mPlaySequence) {
+                    // to be continued
+                }
+                mHandler.sendEmptyMessageDelayed(0, 900);
+            }
+        }; // end thread
     }
 
     @Override
     public void onClick(View view) {
 
+    }
+
+    public void createSequence() {
+        Random randInt = new Random();
+        int ourRandom;
+
+        for (int i = 0; i < mDifficultyLevel; i++) {
+            ourRandom = randInt.nextInt(4) + 1; // ensure non-zero
+            // save to array
+            mSequenceToCopy[i] = ourRandom;
+        }
     }
 }
