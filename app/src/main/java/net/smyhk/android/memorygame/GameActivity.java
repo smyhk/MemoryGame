4f85ff895f5132017539aba192c71f37ebf0e356
Steve Kedzie <seal.team.smyhk@gmail.com>
2017-03-07 16:43:57 -0800

    create MediaPlayer objects

--- app/src/main/java/net/smyhk/android/memorygame/GameActivity.java
+++ app/src/main/java/net/smyhk/android/memorygame/GameActivity.java
@@ -1,13 +1,26 @@
 package net.smyhk.android.memorygame;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        // create media objects
+        final MediaPlayer mp1 = MediaPlayer.create(this, R.raw.button_1);
+        final MediaPlayer mp2 = MediaPlayer.create(this, R.raw.button_2);
+        final MediaPlayer mp3 = MediaPlayer.create(this, R.raw.button_3);
+        final MediaPlayer mp4 = MediaPlayer.create(this, R.raw.button_4);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
